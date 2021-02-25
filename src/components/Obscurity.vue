876a81ed537cee97b9d56fa5f521d4a520d96e48
Manuel Quarneti <manuelquarneti@gmail.com>
2021-02-25 16:35:17 +0100

    obscurity styling

--- src/components/Obscurity.vue
+++ src/components/Obscurity.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center font-semibold">
-    <h2 class="text-2xl mb-4 mt-10" v-text="title"></h2>
+    <h2 class="text-2xl my-10" v-text="title"></h2>
     <div class="text-7xl" v-text="obscurityRating"></div>
   </div>
 </template>
