4cd5e4d7b26ca570fe933dfb32d087c7146c95f9
Manuel Quarneti <manuelquarneti@gmail.com>
2021-03-27 16:29:30 +0100

    share stats

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -76,10 +76,25 @@
           </div>
         </transition>
       </div>
-      <a :href="baseURL">
+      <button @click="shareURL()" class="h-6 w-6">
+        <!-- heroicons: share -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
+          />
+        </svg>
+      </button>
+      <a :href="baseURL" class="h-6 w-6">
         <!-- heroicons: logout -->
         <svg
-          class="h-6 w-6"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
@@ -132,6 +147,18 @@ export default defineComponent({
       emit('setTimeRange', timeRange);
       isTimeRangeDropdownOpen.value = false;
     }
+    const shareURL = () => {
+      if (navigator.share) {
+        navigator.share({
+          title: 'top-for-spotify',
+          text: `${displayName.value}'s Spotify stats`,
+          url: window.location.toString(),
+        });
+      } else {
+        navigator.clipboard.writeText(window.location.toString());
+        window.alert("URL copied to clipboard");
+      }
+    }
 
     onMounted(getDisplayName);
 
@@ -140,6 +167,7 @@ export default defineComponent({
       baseURL,
       isTimeRangeDropdownOpen,
       setTimeRange,
+      shareURL,
     };
   },
 });
