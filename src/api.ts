a571a4e5cbfa26914bd4231f571f987e8c9f0261
Manuel Quarneti <manuelquarneti@gmail.com>
2021-03-29 11:51:09 +0200

    port Obscurity to script setup

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,5 @@
+import { RawTracks } from "../types/top-for-spotify";
+
 export const getHeaders = () => {
   const matches = /#access_token=(.*?)&/.exec(location.hash);
   return matches !== null
@@ -11,4 +13,24 @@ export const getDisplayName = async (headers: Headers) => {
   });
   const j = await response.json();
   return j.display_name;
+}
+
+const parseObscurityRating = (tracks: RawTracks) => {
+  const obscurities: number[] = [];
+
+  tracks.items.forEach((track) => obscurities.push(100 - track.popularity));
+
+  const sum = obscurities.reduce((a, b) => a + b, 0);
+  const avg = sum / obscurities.length || 0;
+
+  return `${Math.round(avg)} %`;
+};
+
+export const getObscurityRating = async (headers: Headers, timeRange: string) => {
+  const response = await fetch(
+    `https://api.spotify.com/v1/me/top/tracks?time_range=${timeRange}`,
+    { headers: headers }
+  );
+  const j = await response.json();
+  return parseObscurityRating(j);
 }
--- src/components/Obscurity.vue
+++ src/components/Obscurity.vue
@@ -1,3 +1,28 @@
+<script setup lang="ts">
+import { defineProps, ref, onMounted, toRefs, watch } from 'vue';
+import { getObscurityRating } from "../api";
+
+const props = defineProps({
+  timeRange: {
+    type: String,
+    required: true,
+  },
+  headers: {
+    type: Headers,
+    required: true,
+  },
+});
+
+const { timeRange } = toRefs(props);
+
+const obscurityRating = ref('? %');
+const updateObscurityRating = () => getObscurityRating(props.headers, props.timeRange).then(r => obscurityRating.value = r);
+
+onMounted(updateObscurityRating);
+
+watch(timeRange, updateObscurityRating);
+</script>
+
 <template>
   <div class="font-semibold flex flex-col">
     <h2
@@ -6,52 +31,3 @@
     <div class="text-9xl">{{ obscurityRating }}</div>
   </div>
 </template>
-
-<script lang="ts">
-import type { RawTracks } from "../../types/top-for-spotify";
-import { defineComponent, onMounted, ref, toRefs, watch } from "vue";
-
-const parseObscurityRating = (tracks: RawTracks) => {
-  const obscurities: number[] = [];
-
-  tracks.items.forEach((track) => obscurities.push(100 - track.popularity));
-
-  const sum = obscurities.reduce((a, b) => a + b, 0);
-  const avg = sum / obscurities.length || 0;
-
-  return `${Math.round(avg)} %`;
-};
-
-export default defineComponent({
-  name: "Obscurity",
-  props: {
-    timeRange: {
-      type: String,
-      required: true,
-    },
-    headers: {
-      type: Headers,
-      required: true,
-    },
-  },
-  setup(props) {
-    const { timeRange } = toRefs(props);
-
-    const obscurityRating = ref('? %');
-    const getObscurityRating = async () => {
-      const response = await fetch(
-        `https://api.spotify.com/v1/me/top/tracks?time_range=${props.timeRange}`,
-        { headers: props.headers }
-      );
-      const j = await response.json();
-      obscurityRating.value = parseObscurityRating(j);
-    }
-
-    onMounted(getObscurityRating);
-
-    watch(timeRange, getObscurityRating);
-
-    return { obscurityRating };
-  },
-});
-</script>
