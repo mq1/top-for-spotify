8046328c716999ac3e6fcd9345dddc07cd86d68c
Manuel Quarneti <manuelquarneti@gmail.com>
2021-04-01 15:46:24 +0200

    use vite-plugin-icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ViteComponents from 'vite-plugin-components'
 import WindiCSS from 'vite-plugin-windicss'
+import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    ViteComponents(),
+    ViteComponents({
+      customComponentResolvers: [
+        ViteIconsResolver({
+          componentPrefix: '',
+          enabledCollections: ['heroicons-outline'],
+        }),
+      ],
+    }),
     WindiCSS(),
+    ViteIcons(),
   ],
   base: '/top-for-spotify/',
   server: {
