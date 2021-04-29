d014ef88bfe9ee7237409160e2372c57de138f7e
Manuel Quarneti <manuelquarneti@gmail.com>
2021-04-29 21:42:08 +0200

    remove inter font

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import generatedRoutes from 'virtual:generated-pages'
 import { setupLayouts } from 'layouts-generated'
 import App from './App.vue'
 import 'virtual:windi.css'
-import 'inter-ui/inter.css'
 
 const routes = setupLayouts(generatedRoutes)
 
