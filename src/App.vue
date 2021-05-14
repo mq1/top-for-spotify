54f5e15225e99910648fb582d30d29cbacf462d5
Manuel Quarneti <manuelquarneti@gmail.com>
2021-05-14 13:14:48 +0200

    move head properties to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+import { useHead } from '@vueuse/head'
+
+useHead({
+  title: 'Top For Spotify',
+  meta: [
+    { name: 'description', content: 'A simple Spotify stats viewer' },
+  ],
+})
+</script>
+
 <template>
   <router-view />
 </template>
