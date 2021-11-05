ec0f27595810132c472b24e75b21c86a9ea85627
Manuel Quarneti <manuelquarneti@gmail.com>
2021-11-05 11:52:07 +0100

    refactoring

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,6 @@
 import { useLocalStorage } from '@vueuse/core'
-import type { Ref } from 'vue'
 import type { TimeRange } from '~/types'
 
 export const locale = useLocalStorage('locale', 'en')
-export const timeRange = useLocalStorage('timeRange', 'short_term') as Ref<TimeRange>
+export const timeRange = useLocalStorage('timeRange', 'short_term' as TimeRange)
 export const spotifyToken = useLocalStorage('spotifyToken', '')
