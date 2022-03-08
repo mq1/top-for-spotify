b27737954e39b7463f05dc7deb75693686c7ade9
Manuel Quarneti <manuelquarneti@gmail.com>
2022-03-08 16:40:00 +0100

    layout fixes

--- src/components/Mood.vue
+++ src/components/Mood.vue
@@ -14,22 +14,22 @@ onMounted(updateAvgFeatures);
   >
     <h2 class="text-5xl sm:text-7xl">Mood</h2>
     <div class="flex flex-col gap-y-4">
-      <div class="flex items-center gap-x-8 text-2xl sm:text-4xl">
+      <div class="flex items-center gap-x-4 sm:gap-x-8 text-xl sm:text-4xl">
         <div class="animate-wobble animate-infinite">🎸</div>
         <div>{{ avgFeatures?.acousticness }}%</div>
         <div>Acousticness</div>
       </div>
-      <div class="flex items-center gap-x-8 text-2xl sm:text-4xl">
+      <div class="flex items-center gap-x-4 sm:gap-x-8 text-xl sm:text-4xl">
         <div class="animate-bounce animate-infinite">💃</div>
         <div>{{ avgFeatures?.danceability }}%</div>
         <div>Danceability</div>
       </div>
-      <div class="flex items-center gap-x-8 text-2xl sm:text-4xl">
+      <div class="flex items-center gap-x-4 sm:gap-x-8 text-xl sm:text-4xl">
         <div class="animate-heartBeat animate-infinite">⚡</div>
         <div>{{ avgFeatures?.energy }}%</div>
         <div>Energy</div>
       </div>
-      <div class="flex items-center gap-x-8 text-2xl sm:text-4xl">
+      <div class="flex items-center gap-x-4 sm:gap-x-8 text-xl sm:text-4xl">
         <div class="animate-swing animate-infinite">😊</div>
         <div>{{ avgFeatures?.valence }}%</div>
         <div>Valence</div>
