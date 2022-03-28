1a0709cd843132fca1a3d1e00b4cf79cd88bbd6a
Manuel Quarneti <manuelquarneti@gmail.com>
2022-03-28 09:27:48 +0200

    update astro to 0.25.2

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
 export default defineConfig({
   buildOptions: {
     site: "https://top-for-spotify.mq1.eu",
   },
-  renderers: ["@astrojs/renderer-preact"],
+  integrations: [preact()],
   vite: {
     ssr: {
       external: ["svgo"],
