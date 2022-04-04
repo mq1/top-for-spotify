a9457d5f74f714830d74b854b9fad709ceabd768
Manuel Quarneti <manuelquarneti@gmail.com>
2022-04-04 10:00:53 +0200

    update astro and @astrojs/tailwind

--- astro.config.ts
+++ astro.config.ts
@@ -3,10 +3,8 @@ import preact from "@astrojs/preact";
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  buildOptions: {
-    site: "https://top-for-spotify.mq1.eu",
-  },
   integrations: [preact(), tailwind({})],
+  site: "https://top-for-spotify.mq1.eu/",
   vite: {
     ssr: {
       external: ["svgo"],
