11f15ec46146453d900fb80346a5a82da3c0bec5
Manuel Quarneti <manuelquarneti@gmail.com>
2022-06-24 16:08:25 +0200

    📄 Update license note

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <footer class="flex flex-col sm:flex-row flex-wrap sm:divide-x items-center mb-4">
-	<div class="px-2">MIT Licensed</div>
+	<div class="px-2">GPL-3.0 Licensed</div>
 	<div class="px-2">© 2021-2022 Manuel Quarneti</div>
 	<div class="px-2">
 		<a class="block" href="https://github.com/mq1/top-for-spotify">
