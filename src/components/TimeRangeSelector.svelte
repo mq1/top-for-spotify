90028267e9761790d0722b0c245bd51cdcd2494c
Manuel Quarneti <manuelquarneti@gmail.com>
2022-08-19 14:39:51 +0200

    💄 Dark mode support

--- src/components/TimeRangeSelector.svelte
+++ src/components/TimeRangeSelector.svelte
@@ -2,7 +2,7 @@
 	import { timeRange } from '../stores/timeRange';
 </script>
 
-<select bind:value={$timeRange} class="rounded-xl focus:border-green-500 focus:ring-green-500">
+<select bind:value={$timeRange} class="rounded-xl focus:border-green-500 focus:ring-green-500 dark:bg-black">
 	<option value="short_term">Short Term</option>
 	<option value="long_term">Long Term</option>
 </select>
