1d453bb70cdc85e874fc19abbbac1a93a72a7460
Manuel Quarneti <manuelquarneti@gmail.com>
2022-08-23 18:05:45 +0200

    🐛

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -3,7 +3,8 @@ import html2canvas from "html2canvas";
 
 const share = async (id: string) => {
     const canvas = await html2canvas(document.getElementById(id)!);
-    const dataUrl = canvas.toBlob(async (blob) => {
+
+    canvas.toBlob(async (blob) => {
         const files = [new File([blob!], 'image.png', { type: blob!.type })]
         const shareData = {
             files,
