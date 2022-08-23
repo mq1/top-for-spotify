b92cd28b2cb34fe63d0e0eeaef045ca9c41f08a2
Manuel Quarneti <manuelquarneti@gmail.com>
2022-08-23 19:57:34 +0200

    🐛 Fix share

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -3,20 +3,10 @@ import html2canvas from "html2canvas";
 const share = async (id: string) => {
     const canvas = await html2canvas(document.getElementById(id)!);
     const dataUrl = canvas.toDataURL();
-    const blob = await (await fetch(dataUrl)).blob();
-    const image = new File([blob], `${id}.png`, { type: blob.type });
 
-    if (navigator.canShare && navigator.canShare({ files: [image] })) {
-        await navigator.share({
-            files: [image],
-            title: id,
-        });
-    } else {
-        let link = document.createElement("a");
-        link.download = `${id}.png`;
-        link.href = dataUrl;
-        link.click();
-    }
+    let link = document.createElement("a");
+    link.href = dataUrl;
+    link.click();
 };
 
 export default share;
