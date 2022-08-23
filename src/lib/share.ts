fdb52e60df5e6252cbe32a5ffb7aeca46c9c6241
Manuel Quarneti <manuelquarneti@gmail.com>
2022-08-23 17:55:31 +0200

    ⚡️ Compress images

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -3,23 +3,21 @@ import html2canvas from "html2canvas";
 
 const share = async (id: string) => {
     const canvas = await html2canvas(document.getElementById(id)!);
+    const dataUrl = canvas.toDataURL("image/jpeg", 0.95);
+    const blob = await fetch(dataUrl).then((r) => r.blob());
+    const files = [new File([blob!], `${id}.jpeg`, { type: "image/jpeg" })];
 
-    if (!navigator.canShare) {
-        const dataUrl = canvas.toDataURL("image/png");
+    if (navigator.canShare && navigator.canShare({ files })) {
+        navigator.share({
+            files,
+            title: `My ${id}`,
+            text: `My ${id}`,
+        });
+    } else {
         let link = document.createElement('a');
         link.download = `${id}.png`;
         link.href = dataUrl;
         link.click();
-    } else {
-        canvas.toBlob(async (blob) => {
-            const files = [new File([blob!], `${id}.png`, { type: "image/png" })];
-
-            navigator.share({
-                files: files,
-                title: `My ${id}`,
-                text: `My ${id}`,
-            });
-        })
     }
 };
 
