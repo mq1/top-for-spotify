fe9fd71fce18e61c3fd234ebdb3713cf6905ae59
Manuel Quarneti <manuelquarneti@gmail.com>
2022-08-23 15:58:14 +0200

    ♻️ Refactor time range selector

--- src/components/TimeRangeSelector.svelte
+++ src/components/TimeRangeSelector.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <select bind:value={$timeRange} class="rounded-xl focus:border-green-500 focus:ring-green-500 dark:bg-black">
-	<option value="short_term">Short Term</option>
-	<option value="long_term">Long Term</option>
+	<option value="short_term">Last Month</option>
+	<option value="medium_term">Last 6 Months</option>
+	<option value="long_term">All Time</option>
 </select>
--- src/stores/timeRange.ts
+++ src/stores/timeRange.ts
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
 
-type TimeRange = 'short_term' | 'long_term';
+type TimeRange = 'short_term' | 'medium_term' | 'long_term';
 
-export const timeRange = writable<TimeRange>('long_term');
+export const timeRange = writable<TimeRange>('short_term');
